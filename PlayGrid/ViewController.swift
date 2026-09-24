0466a24a6cb5d37dba628ab891929872028a403b
Uriel <uriel22_18@protonmail.com>
2026-09-24 08:37:35 -0600

    Primer commit

---
+++ PlayGrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayGrid
+//
+//  Created by Facultad de Contaduría y Administración on 24/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
